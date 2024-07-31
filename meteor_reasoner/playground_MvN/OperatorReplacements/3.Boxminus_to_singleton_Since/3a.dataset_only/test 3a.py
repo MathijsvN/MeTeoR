559325528471c5906f804d172d2dcc13f76eee0e
mvnoort <Mathijs.vanNoort@UGent.be>
2024-07-31 17:19:00 +0200

    Added test 3: Boxminus to singleton Since

---
+++ meteor_reasoner/playground_MvN/OperatorReplacements/3.Boxminus_to_singleton_Since/3a.dataset_only/test 3a.py
@@ -0,0 +1,78 @@
+from meteor_reasoner.materialization.materialize import *
+from meteor_reasoner.utils.loader import load_dataset, load_program
+
+
+# Original Syntax
+
+print("ORIGINAL SYNTAX")
+
+with open("meteor_reasoner/playground_MvN/OperatorReplacements/3.Boxminus_to_singleton_Since/3a.dataset_only/data/original/dataset_original.txt") as file:
+    data = file.readlines()
+with open("meteor_reasoner/playground_MvN/OperatorReplacements/3.Boxminus_to_singleton_Since/3a.dataset_only/data/original/program_original.txt") as file:
+    program = file.readlines()
+
+D = load_dataset(data)
+D_index = build_index(D)
+rules = load_program(program)
+for i in range(1,2):
+    print("Iteration:", i)
+    print("=====================")
+    print("Before D:")
+    print_dataset(D)
+    print("Derived facts:")
+    delta_new = naive_immediate_consequence_operator(rules, D, D_index)
+    print_dataset(delta_new)
+    print("After D:")
+    materialize(D, rules, K=1)
+    print_dataset(D)
+
+
+# Replacement Syntax: Boxminus to singleton Diamondminus and Since
+
+print("REPLACEMENT SYNTAX: Boxminus to singleton Diamondminus and Since")
+
+with open("meteor_reasoner/playground_MvN/OperatorReplacements/3.Boxminus_to_singleton_Since/3a.dataset_only/data/replaced/dataset_replaced_i.txt") as file:
+    data = file.readlines()
+with open("meteor_reasoner/playground_MvN/OperatorReplacements/3.Boxminus_to_singleton_Since/3a.dataset_only/data/replaced/program_replaced_i.txt") as file:
+    program = file.readlines()
+
+D = load_dataset(data)
+D_index = build_index(D)
+rules = load_program(program)
+for i in range(1,2):
+    print("Iteration:", i)
+    print("=====================")
+    print("Before D:")
+    print_dataset(D)
+    print("Derived facts:")
+    delta_new = naive_immediate_consequence_operator(rules, D, D_index)
+    print_dataset(delta_new)
+    print("After D:")
+    materialize(D, rules, K=1)
+    print_dataset(D)
+
+
+
+# Replacement Syntax: Boxminus to singleton Since
+
+print("REPLACEMENT SYNTAX: Boxminus to singleton Since")
+
+with open("meteor_reasoner/playground_MvN/OperatorReplacements/3.Boxminus_to_singleton_Since/3a.dataset_only/data/replaced/dataset_replaced_ii.txt") as file:
+    data = file.readlines()
+with open("meteor_reasoner/playground_MvN/OperatorReplacements/3.Boxminus_to_singleton_Since/3a.dataset_only/data/replaced/program_replaced_ii.txt") as file:
+    program = file.readlines()
+
+D = load_dataset(data)
+D_index = build_index(D)
+rules = load_program(program)
+for i in range(1,2):
+    print("Iteration:", i)
+    print("=====================")
+    print("Before D:")
+    print_dataset(D)
+    print("Derived facts:")
+    delta_new = naive_immediate_consequence_operator(rules, D, D_index)
+    print_dataset(delta_new)
+    print("After D:")
+    materialize(D, rules, K=1)
+    print_dataset(D)
